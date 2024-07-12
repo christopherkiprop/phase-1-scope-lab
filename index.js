2090774f146c6fe6ed2195cbbf2f2517903765e3
christopherkiprop <choppachepa@gmail.com>
2024-07-12 23:14:26 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  var leastFavoriteCustomer = 'initial customer';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new customer';
+  }
+  
